8ec2e226099aade55c7735c028ecb3426c59a7a3
Praveen R <praveen2005sanram@gmail.com>
2026-09-04 21:48:42 +0530

    day2 Practice

--- src/Basics_01/Exercise1.java
+++ src/Basics_01/Exercise1.java
@@ -51,5 +51,6 @@ class Employee{
     void display(){
         System.out.println("Employee " + e_no +  " details" + " \nName : " + name +"\nAge : "+ age +"\nSalary : " + salary +"\nDepartment : " + department);
         System.out.println();
+        System.out.println("Hello world");
     }
 }
