fad0b6835ca9979c777c6c91272c9a8587d13c9e
Praveen R <praveen2005sanram@gmail.com>
2026-09-08 23:20:00 +0530

    day4 Practices

---
+++ src/OOPS_02/Inheritance/Simple_Inheritance.java
@@ -0,0 +1,20 @@
+package OOPS_02.Inheritance;
+
+public class Simple_Inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
+
+class Animal{
+    void sound(){
+        System.out.println("Makes Sound");
+    }
+}
+
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Barks");
+    }
+}
